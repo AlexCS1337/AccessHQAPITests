537c3d5bf6f7ed20f488051e020b08c41ac8fec0
Alexander Silva <alexander.carlos.silva@gmail.com>
2022-06-28 14:30:35 +1000

    Create Verify_Correct_Postcode Test

--- src/test/java/com/accesshq/api/TestAPISuite.java
+++ src/test/java/com/accesshq/api/TestAPISuite.java
@@ -3,6 +3,7 @@ package com.accesshq.api;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class TestAPISuite {
 
@@ -16,5 +17,16 @@ public class TestAPISuite {
                 .get("https://digitalapi.auspost.com.au/postcode/search.json")
                 .then().assertThat()
                 .statusCode(200);
+    }
+
+    @Test
+    public void Verify_Correct_Postcode() {
+        given().
+                header("auth-key", "5b37a04c-ca81-4180-b7be-63f36fdf71ad")
+                .param("q", "Tascott")
+                .param("state", "NSW")
+                .when()
+                .get("https://digitalapi.auspost.com.au/postcode/search.json")
+                .then().assertThat().body("localities.locality.postcode",equalTo(2250));
     }
 }
